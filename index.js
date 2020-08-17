a4023ffcf2c576aab7bbbd8ca22e8140222d7467
Kassi Stumb <kassi.mcintosh@gmail.com>
2020-08-17 13:59:50 -0400

    initial commit: package.json

---
+++ index.js
@@ -0,0 +1,19 @@
+const inquirer = require("inquirer");
+const markdownModule = require('./utils/generateMarkdown');
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
